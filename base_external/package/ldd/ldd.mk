bc54bd61a96cb28fc51b2bfdbf30d12efde285f0
JayLooi <jaylks96@gmail.com>
2023-09-09 17:39:46 +0800

    assignment7-part2: revert changes to ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,12 +1,7 @@
-LDD_VERSION = 718cbdf07e082486e008537660e6b9fb4fb07a45
+LDD_VERSION = 4f8d971db1b5dbbff96be487a83206301c700632
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-JayLooi.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
-
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
-endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
