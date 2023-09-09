2b4318348d8e32ef57494da00431de2db4a2ae17
JayLooi <jaylks96@gmail.com>
2023-09-09 17:14:31 +0800

    assignment7-part2: change ldd git commit hash

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,4 +1,4 @@
-LDD_VERSION = 4f8d971db1b5dbbff96be487a83206301c700632
+LDD_VERSION = 718cbdf07e082486e008537660e6b9fb4fb07a45
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-JayLooi.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
