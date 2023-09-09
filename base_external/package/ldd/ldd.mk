dd87eefbdd58d4ce55697929daf71f9a805a11aa
JayLooi <jaylks96@gmail.com>
2023-09-09 18:04:48 +0800

    assignment7-part2: updated ldd.mk according to https://buildroot.org/downloads/manual/manual.html#_infrastructure_for_packages_building_kernel_modules

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -2,6 +2,10 @@ LDD_VERSION = 4f8d971db1b5dbbff96be487a83206301c700632
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-JayLooi.git
 LDD_SITE_METHOD = git
 
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
 
