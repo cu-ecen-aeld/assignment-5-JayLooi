b340f7082ad8c6203207abb286d0f4b16ad10a6a
JayLooi <jaylks96@gmail.com>
2023-09-09 17:04:36 +0800

    assignment7-part2: updated ldd package mk file

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -3,5 +3,11 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-JayLooi.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
+define LDD_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
+
