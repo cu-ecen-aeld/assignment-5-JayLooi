e9b70b4af7d738c49d748e4d6fc4404d3405a622
JayLooi <jaylks96@gmail.com>
2023-09-09 17:47:43 +0800

    assignment7-part2: updated ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,7 +1,6 @@
 LDD_VERSION = 4f8d971db1b5dbbff96be487a83206301c700632
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-JayLooi.git
 LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
