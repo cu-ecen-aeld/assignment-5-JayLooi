1f4295f11339a486b96dd01a0d000618b3196ee2
JayLooi <jaylks96@gmail.com>
2023-09-09 13:59:53 +0800

    assignment7-part2: added ldd package

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,7 @@
+LDD_VERSION = 4f8d971db1b5dbbff96be487a83206301c700632
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-JayLooi.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
